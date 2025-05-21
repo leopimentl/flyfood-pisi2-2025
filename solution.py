33acf0c58cc7c0464ecadebe741af2e4c6d0eaf3
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2025-05-21 10:31:49 -0300

    feat: adiciona um cache para as distancias conhecidas
    função que irá evitar calculos repetidos de distancias

--- solution.py
+++ solution.py
@@ -1,3 +1,5 @@
+cache_distancia = {}
+
 def ler_matriz(caminho_arquivo):
     with open(caminho_arquivo) as c:
         linhas, colunas = [int(x) for x in c.readline().split()]
@@ -9,8 +11,20 @@ def ler_matriz(caminho_arquivo):
                     pontos[linha[j]] = (i, j)
         return pontos
 
-def calcula_distancia(a, b):
+def calcular_distancia(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
+
+def precalcular_distancias(pontos):
+    pontos_entrega = [p for p in pontos.keys() if p != 'R']
+    todos_pontos = pontos_entrega + ['R']
+    
+    for i in range(len(todos_pontos)):
+        for j in range(i+1, len(todos_pontos)):
+            ponto_a = todos_pontos[i]
+            ponto_b = todos_pontos[j]
+            distancia = calcular_distancia(pontos[ponto_a], pontos[ponto_b])
+            cache_distancia[(ponto_a, ponto_b)] = distancia
+            cache_distancia[(ponto_b, ponto_a)] = distancia
 
 def gerar_permutacoes(itens):
     if len(itens) == 0:
@@ -38,10 +52,10 @@ def encontrar_rota_otima(pontos):
         posicao_atual = origem
         
         for ponto in permutacao:
-            distancia_atual += calcula_distancia(posicao_atual, pontos[ponto])
+            distancia_atual += calcular_distancia(posicao_atual, pontos[ponto])
             posicao_atual = pontos[ponto]
 
-        distancia_atual += calcula_distancia(posicao_atual, origem)
+        distancia_atual += calcular_distancia(posicao_atual, origem)
         
         if distancia_atual < menor_distancia:
             menor_distancia = distancia_atual
@@ -52,6 +66,8 @@ def encontrar_rota_otima(pontos):
 def main():
     caminho_arquivo = 'matriz.txt'
     pontos = ler_matriz(caminho_arquivo)
+    precalcular_distancias(pontos)
+    print(cache_distancia)
     rota_otima = encontrar_rota_otima(pontos)
     print(rota_otima)
     
