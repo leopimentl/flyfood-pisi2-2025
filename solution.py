da44d0c8f91cc656c12c9c4fa35593fe8626c8c5
Leandro Khalel <leandrokhalelpimentel@gmail.com>
2025-05-21 09:11:26 -0300

    feat(solution): leitura da matriz para identificar todos os pontos não nulos
    cria a função ler_matriz para ler corretamente todos os pontos dferentes de '0' na matriz, armazenando suas posições em um dicionário.

---
+++ solution.py
@@ -0,0 +1,18 @@
+def ler_matriz(caminho_arquivo):
+    with open(caminho_arquivo) as c:
+        linhas, colunas = [int (x) for x in c.readline().split()]
+        pontos = {'R': None}
+        for i in range(linhas):
+            linha = c.readline().strip().split()
+            for j in range(colunas):
+                if linha[j] != '0':
+                    pontos[linha[j]] = (i, j)
+        return pontos
+    
+def main():
+    caminho_arquivo = 'matriz.txt'
+    pontos = ler_matriz(caminho_arquivo)
+    for i in pontos:
+        print(i, pontos[i])
+        
+main()
